c9ff38840764353b0127cbca4ddcd180659784b6
rbeere <rbeere@nouvex.de>
2018-07-26 15:25:16 +0200

    a pcmu encoder

---
+++ plugins/ulawalaw.h
@@ -0,0 +1,16 @@
+
+unsigned char Snack_Lin2Alaw( short pcm_val);
+
+static short search( short val, short *table,  short size);
+
+unsigned char Snack_Lin2Alaw( short pcm_val);
+ 
+short Snack_Alaw2Lin( unsigned char	a_val);
+
+unsigned char Snack_Lin2Mulaw( short pcm_val);
+
+short Snack_Mulaw2Lin( unsigned char u_val);
+
+unsigned char alaw2ulaw( unsigned char	aval);
+
+unsigned char ulaw2alaw( unsigned char	uval);
